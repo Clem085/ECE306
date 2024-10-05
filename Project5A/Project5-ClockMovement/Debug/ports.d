baf7035e1ba4240b66280f51f12404900bff8d44
Connor <consav04@gmail.com>
2024-10-05 11:12:07 -0400

    Please push to GitHub

---
+++ Project5A/Project5-ClockMovement/Debug/ports.d
@@ -0,0 +1,60 @@
+# FIXED
+
+ports.obj: ../ports.c
+ports.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430fr2355.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/in430.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/legacy.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+ports.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+ports.obj: ../functions.h
+ports.obj: ../LCD.h
+ports.obj: ../ports.h
+ports.obj: ../macros.h
+ports.obj: ../motors.h
+ports.obj: ../Display.h
+ports.obj: ../switch.h
+
+../ports.c:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/in430.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/legacy.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
+../functions.h:
+
+../LCD.h:
+
+../ports.h:
+
+../macros.h:
+
+../motors.h:
+
+../Display.h:
+
+../switch.h:
+
---
+++ [b]-Project5A/Project5-ClockMovement/Debug/init.d
@@ -0,0 +1,51 @@
+# FIXED
+
+init.obj: ../init.c
+init.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430.h
+init.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430fr2355.h
+init.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/in430.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+init.obj: /home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/legacy.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+init.obj: /home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+init.obj: ../functions.h
+init.obj: ../LCD.h
+init.obj: ../ports.h
+init.obj: ../macros.h
+
+../init.c:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/in430.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/connor/ti/ccstheia150/ccs/ccs_base/msp430/include/legacy.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/home/connor/ti/ccstheia150/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
+../functions.h:
+
+../LCD.h:
+
+../ports.h:
+
+../macros.h:
+
