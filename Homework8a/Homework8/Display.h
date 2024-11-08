69eaf30cfa3ea5ff652422857a7ec641a2d0f47b
Connor <consav04@gmail.com>
2024-11-08 14:22:23 -0500

    Added broken code for homework 8

---
+++ Homework8a/Homework8/Display.h
@@ -0,0 +1,17 @@
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+
+// Globals
+char backlight_status;
+
+// Functions
+void backlight_control(void);
+//void dispPrint(char *, char *, char *, char *);
+
+
+
+void dispPrint(char *line, char lineToUpdate);
+int middleChar(char * string);
+void adc_line(char line, char location);
+#endif /* DISPLAY_H_ */
